ad52c2930aeeb0dd3ba688e943cc53a8937c5969
hshelm <haydenshelm@gmail.com>
2025-05-21 17:55:51 +0000

    add behavioral

--- dkps/dkps.py
+++ dkps/dkps.py
@@ -1,9 +1,57 @@
 import numpy as np
 from graspologic.embed import OmnibusEmbed, ClassicalMDS
 
+from scipy.spatial.distance import pdist, squareform
+
 from .utils import knn_graph
 
+
 class DataKernelPerspectiveSpace:
+    def __init__(self,
+                response_distribution_fn=np.mean,
+                response_distribution_axis=1,
+                metric_cmds='euclidean',
+                n_components_cmds=None,
+                n_elbows_cmds=2,
+                ):
+        self.response_distribution_fn=response_distribution_fn
+        self.response_distribution_axis=response_distribution_axis
+        self.metric_cmds=metric_cmds
+        self.n_components_cmds=n_components_cmds
+        self.n_elbows_cmds=n_elbows_cmds
+
+
+    def fit_transform(self, X):
+        if isinstance(X, dict):
+            self.model_list=list(X.keys())
+            X = list(X.values())
+
+        
+        if isinstance(X, (list, np.ndarray)):
+            self.model_list=None
+            n_queries = len(X[0])
+            for i,x in enumerate(X):
+                x=np.array(x)
+                assert x.ndim >= 2
+                assert x.shape[0] == n_queries
+
+                X[i]=self.response_distribution_fn(x, self.response_distribution_axis)
+                    
+        else:
+            raise ValueError('X must be a dict of array-like values or array-like with elements with at least 2 dimensions')
+
+        X = np.array(X)
+        #- this only works in cases where representation of models are matrices
+        dist_matrix = squareform(pdist(X.reshape(len(X), -1), metric=self.metric_cmds)) / np.sqrt(n_queries)
+        cmds_embds = ClassicalMDS(self.n_components_cmds, self.n_elbows_cmds).fit_transform(dist_matrix)
+        
+        if self.model_list:
+            return {key: cmds_embds[i] for i, key in enumerate(self.model_list)}
+        
+        return cmds_embds
+
+
+class DataKernelFunctionalSpace:
     def __init__(self, n_neighbors=None, n_components_joint=None, n_elbows_joint=2, n_components_cmds=None, n_elbows_cmds=2, metric='euclidean'):
         self.n_neighbors=n_neighbors
         
